2ee05a415539e147802bd46d9dea6feea97d4cbc
Orlov-D <82273356+Orlov-D@users.noreply.github.com>
2021-05-13 23:06:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ public class Main {
         int balance = 100;
         int bonus;
         int transfer = 1010;
-        if (transfer > 1000) if (transfer % 100 == 0)
-            bonus = transfer / 100;
-        else
-            bonus = (transfer - transfer % 100) / 100;
-        else
+        if (transfer > 1000) {
+            if (transfer % 100 == 0) {
+                bonus = transfer / 100;
+            } else {
+                bonus = (transfer - transfer % 100) / 100;
+            }
+        } else {
             bonus = 0;
+        }
         balance = balance + bonus + transfer;
         System.out.println("Баланс= " + balance + " Бонус= " + bonus);
     }
